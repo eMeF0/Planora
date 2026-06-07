da69c1fbf2d1f710cdefadb1d1fddf5ade0f4f07
eMeF0 <dark66011101@gmail.com>
2026-06-07 22:41:18 +0200

    feat: Add all create pages(dtos)

--- Planora.Web/Models/DTOs/Task/CreateTaskDto.cs
+++ Planora.Web/Models/DTOs/Task/CreateTaskDto.cs
@@ -2,5 +2,8 @@
 {
     public class CreateTaskDto
     {
+        public string Title {get;set;} = string.Empty;
+        public string? Description {get;set;}
+        public DateTimeOffset? Deadline {get;set;}
     }
 }
